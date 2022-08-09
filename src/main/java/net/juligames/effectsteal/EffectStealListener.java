d14ddbdde8268bae6888a9eba9d984ec1cdcef69
TDR <tdr.minecraft@gmail.com>
2022-08-09 14:44:34 +0200

    Update EffectStealListener.java

--- src/main/java/net/juligames/effectsteal/EffectStealListener.java
+++ src/main/java/net/juligames/effectsteal/EffectStealListener.java
@@ -1,12 +1,25 @@
 package net.juligames.effectsteal;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.jetbrains.annotations.NotNull;
 
 public final class EffectStealListener implements Listener {
 
     public EffectStealListener(@NotNull EffectSteal plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+
+    @EventHandler
+    public void onKill(@NotNull PlayerDeathEvent deathEvent) {
+        Player player = deathEvent.getPlayer();
+        Player killer = player.getKiller();
+        if(killer == null) return;
+        EffectSteal.log(player.getName() + " was killed by " + killer);
+
     }
 
 
