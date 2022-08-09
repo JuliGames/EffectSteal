1cad5436c6e0c1e91a16e81425afb248d357940a
TDR <tdr.minecraft@gmail.com>
2022-08-09 14:05:17 +0200

    add static way to access plugin

--- src/main/java/net/juligames/effectsteal/EffectSteal.java
+++ src/main/java/net/juligames/effectsteal/EffectSteal.java
@@ -2,13 +2,22 @@ package net.juligames.effectsteal;
 
 import net.juligames.effectsteal.util.EffectMap;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.initialization.qual.UnknownInitialization;
 
 public final class EffectSteal extends JavaPlugin {
 
+    @UnknownInitialization
+    public static EffectSteal get() { return plugin;}
+
+
     private final EffectMap effectMap = new EffectMap();
+
+    @UnknownInitialization
+    private static EffectSteal plugin;
 
     @Override
     public void onEnable() {
+        plugin = this;
         // Plugin startup logic
         EffectStealListener effectStealListener = new EffectStealListener(this);
 
@@ -22,6 +31,5 @@ public final class EffectSteal extends JavaPlugin {
     public EffectMap getEffectMap() {
         return effectMap;
     }
-
 
 }
