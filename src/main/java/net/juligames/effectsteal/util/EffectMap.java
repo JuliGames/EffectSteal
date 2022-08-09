ba903d75743eb525aab5da82e5206105a05e9a7c
TDR <tdr.minecraft@gmail.com>
2022-08-09 14:38:22 +0200

    add plus() minus()

--- src/main/java/net/juligames/effectsteal/util/EffectMap.java
+++ src/main/java/net/juligames/effectsteal/util/EffectMap.java
@@ -5,6 +5,24 @@ import java.util.UUID;
 
 public final class EffectMap extends HashMap<UUID,EffectArrayList> {
 
+
+    public void plus(UUID uuid) {
+
+    }
+
+    public void minus(UUID uuid) {
+
+    }
+
+    /**
+     *
+     * @param uuid
+     * @return the resulting EffectArrayList from the Map
+     */
+    public EffectArrayList create(UUID uuid) {
+        return put(uuid, new EffectArrayList());
+    }
+
 }
 
 
