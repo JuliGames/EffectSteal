1e22759826000ad13dfa20ec488aca82a971f71e
TDR <tdr.minecraft@gmail.com>
2022-08-09 14:44:40 +0200

    add log()

--- src/main/java/net/juligames/effectsteal/EffectSteal.java
+++ src/main/java/net/juligames/effectsteal/EffectSteal.java
@@ -3,11 +3,19 @@ package net.juligames.effectsteal;
 import net.juligames.effectsteal.util.EffectMap;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.initialization.qual.UnknownInitialization;
+import org.checkerframework.common.initializedfields.qual.EnsuresInitializedFields;
+
+import java.util.logging.Level;
 
 public final class EffectSteal extends JavaPlugin {
 
     @UnknownInitialization
     public static EffectSteal get() { return plugin;}
+
+    public static void log(String s) {
+        if(get() == null) System.out.println(s); else
+        get().getLogger().log(Level.INFO,s);
+    }
 
 
     private final EffectMap effectMap = new EffectMap();
